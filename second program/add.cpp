f7235bcc768939a99539f18f5858180062a41088
unknown <you@example.com>
2019-10-09 22:12:05 +0600

    second program

---
+++ second program/add.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  int firstNumber;
+  int secondNumber;
+  int result;
+  cout<<"please enter first number:"<<endl;
+  cin>>firstNumber;
+  cout<<"please enter second number: "<<endl;
+  cin>>secondNumber;
+  result = firstNumber + secondNumber;
+  cout<<"the addition of two number is:"<<result;
+  return 0;
+}
