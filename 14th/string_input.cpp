c4a31a710af9e7d132d7f4effe315b01efc9d5bd
unknown <you@example.com>
2019-10-10 19:03:40 +0600

    string input in c++

---
+++ 14th/string_input.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+//string input in c++
+
+int main(){
+
+  char name[20];
+  cout<<"Please enter your name:"<<endl;
+  gets(name);
+  cout<<"Your name is: "<<name;
+
+
+
+  return 0;
+}
