a23c9f045bb3adc58d696bfe956cd4c41622e225
unknown <you@example.com>
2019-10-12 09:47:48 +0600

    create class file in different directory

---
+++ 20th/firstproject/myfirstclass.cpp
@@ -0,0 +1,13 @@
+#include "myfirstclass.h"
+#include<iostream>
+using namespace std;
+
+myFirstclass::myFirstclass()
+{
+    cout<<"inside the constructor"<<endl;
+}
+
+void myFirstclass::display(){
+
+   cout<<"inside the display function"<<endl;
+}
---
+++ 20th/firstproject/myfirstclass.h
@@ -0,0 +1,17 @@
+#ifndef MYFIRSTCLASS_H
+#define MYFIRSTCLASS_H
+#include "myfirstclass.h"
+
+
+class myFirstclass
+{
+    public:
+        myFirstclass();
+        void display();
+
+    protected:
+
+    private:
+};
+
+#endif // MYFIRSTCLASS_H
