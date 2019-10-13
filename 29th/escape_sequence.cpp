e0df89f3bb2b20607a63121cc31a57bc572ffdf2
unknown <you@example.com>
2019-10-13 12:43:07 +0600

    escape sequence in c++

---
+++ 29th/escape_sequence.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+//escape sequence in c++
+
+
+int main(){
+
+
+    //New Line (\n)
+    //Tab (\t)
+    //Alert Bell (\a)
+    //Backspace (\b)
+    //Single Quote (\’)
+    //Carriage Return (\r)
+    //Form Feed (\f)
+
+
+    cout<<"COMPUTER\nSCIENCE"<<endl;
+    cout<<"COMPUTER\tSCIENCE"<<endl;
+    cout<<"COMPUTER\aSCIENCE"<<endl;
+    cout<<"COMPUTER\bSCIENCE"<<endl;
+    cout<<"\'COMPUTER SCIENCE\'"<<endl;
+    cout<<"COMPUTER\rSCIENCE"<<endl;
+    cout<<"COMPUTER\fSCIENCE"<<endl;
+
+    getch();
+}
