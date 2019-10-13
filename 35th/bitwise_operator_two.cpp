6af0e91e46810bb8e154c0c46bfacfd4b9db8ba5
unknown <you@example.com>
2019-10-13 23:05:11 +0600

    bitwise operator two

---
+++ 35th/bitwise_operator_two.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+
+//bitwise operator
+
+using namespace std;
+
+int main(){
+
+
+  int a = 32;
+  int c = a>>3;
+
+  cout<<c<<endl;
+
+  int d = a<<2;
+
+  cout << d << endl;
+
+
+   getch();
+}
